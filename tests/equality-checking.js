ae372a64a82398bdb406fba73b4e9f336f6d16d0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-15 12:25:21 -0500

    This one is way better

--- tests/equality-checking.js
+++ tests/equality-checking.js
@@ -1,54 +1,58 @@
-import { Bench } from 'tinybench'
-
-const bench = new Bench({
-  name: 'equality checking', time: 100,
-  setup: (_task, mode) => {
-    // Run the garbage collector before warmup at each cycle
-    if (mode === 'warmup' && typeof globalThis.gc === 'function') {
-      globalThis.gc()
-    }
-  },
-});
+import { run, bench, boxplot, summary, do_not_optimize } from 'mitata';
 
 let vTrue = true;
 let vFalse = false;
 
-bench.add('if ()', () => {
-  if (vTrue) { }
-  if (true) { }
-  if (false) { }
-  if (vFalse) { }
-}).add('if (!()', () => {
-  if (!vTrue) { }
-  if (!true) { }
-  if (!false) { }
-  if (!vFalse) { }
-}).add('if (x === true)', () => {
-  if (vTrue === true) { }
-  if (true === true) { }
-  if (false === true) { }
-  if (vFalse === true) { }
-}).add('if (x == true)', () => {
-  if (vTrue == true) { }
-  if (true == true) { }
-  if (false == true) { }
-  if (vFalse == true) { }
-}).add('if (x === false)', () => {
-  if (vTrue === false) { }
-  if (true === false) { }
-  if (false === false) { }
-  if (vFalse === false) { }
-}).add('if (x == false)', () => {
-  if (vTrue == false) { }
-  if (true == false) { }
-  if (false == false) { }
-  if (vFalse == false) { }
-})
+/**
+  * NOTE: JIT optimization is at play
+  *       https://www.npmjs.com/package/mitata
+  *
+  *       Try to de-optimize
+  */
 
-  ;
+const go = (name, x) => boxplot(() => void summary(() => void bench(name, function*() { yield x })))
 
-await bench.run();
+summary(() => {
+  bench('if ()', (() => {
+    if (vTrue) { }
+    if (true) { }
+    if (false) { }
+    if (vFalse) { }
+  }))
 
-console.log(bench.name)
-console.table(bench.table())
+  bench('if (!()', () => {
+    if (!vTrue) { }
+    if (!true) { }
+    if (!false) { }
+    if (!vFalse) { }
+  })
+  bench('if (x === true)', () => {
+    if (vTrue === true) { }
+    if (true === true) { }
+    if (false === true) { }
+    if (vFalse === true) { }
+  })
+  bench('if (x == true)', () => {
+    if (vTrue == true) { }
+    if (true == true) { }
+    if (false == true) { }
+    if (vFalse == true) { }
+  })
 
+  bench('if (x === false)', () => {
+    if (vTrue === false) { }
+    if (true === false) { }
+    if (false === false) { }
+    if (vFalse === false) { }
+  })
+
+  bench('if (x == false)', () => {
+    if (vTrue == false) { }
+    if (true == false) { }
+    if (false == false) { }
+    if (vFalse == false) { }
+  })
+});
+
+
+await run();
--- tests/glimmer-vm-isElement.js
+++ tests/glimmer-vm-isElement.js
@@ -1,76 +1,24 @@
-import { Suite, chartReport } from 'bench-node';
-import { Bench } from 'tinybench'
-
-const workerSuite = new Suite({
-  reporter: chartReport,
-  // requires no shared data between workers
-  useWorkers: true,
-});
-
-const suite = new Suite({
-  reporter: chartReport,
-});
+import { run, bench, boxplot, summary, do_not_optimize } from 'mitata';
 
 const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
 const rDefault = /^<([\d\-a-z][\d\-A-Za-z]*)>$/;
 const rU = /^<([\d\-a-z][\d\-A-Za-z]*)>$/u;
 const rV = /^<([\d\-a-z][\d\-A-Za-z]*)>$/v;
 
+boxplot(() => {
+  bench('<regex>', () => {
+    return do_not_optimize(elements.forEach(d => rDefault.exec(d)));
+  }).gc('inner');
 
-workerSuite.add('<regex>', () => {
-  const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
-  const rDefault = /^<([\d\-a-z][\d\-A-Za-z]*)>$/;
-  elements.forEach(d => rDefault.exec(d))
-});
-workerSuite.add('<regex>/u', () => {
-  const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
-  const rU = /^<([\d\-a-z][\d\-A-Za-z]*)>$/u;
-  elements.forEach(d => rU.exec(d))
-});
-workerSuite.add('<regex>/v', () => {
-  const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
-  const rV = /^<([\d\-a-z][\d\-A-Za-z]*)>$/v;
-  elements.forEach(d => rV.exec(d))
+  bench('<regex>/u', () => {
+    return do_not_optimize(elements.forEach(d => rU.exec(d)));
+  }).gc('inner');
+
+  bench('<regex>/v', () => {
+    return do_not_optimize(elements.forEach(d => rV.exec(d)));
+  }).gc('inner');
+
 });
 
 
-suite.add('<regex>', () => {
-  elements.forEach(d => rDefault.exec(d))
-});
-suite.add('<regex>/u', () => {
-  elements.forEach(d => rU.exec(d))
-});
-suite.add('<regex>/v', () => {
-  elements.forEach(d => rV.exec(d))
-});
-
-// console.log('\n\nIn workers');
-// await workerSuite.run()
-//
-// console.log('\n\nNot in workers');
-// await suite.run();
-//
-console.log('\n\nwith tinybench');
-const bench = new Bench({
-  name: 'regex flag comparison', time: 100,
-  setup: (_task, mode) => {
-    // Run the garbage collector before warmup at each cycle
-    if (mode === 'warmup' && typeof globalThis.gc === 'function') {
-      globalThis.gc()
-    }
-  },
-});
-
-bench.add('<regex>', () => {
-  elements.forEach(d => rDefault.exec(d))
-}).add('<regex>/u', () => {
-  elements.forEach(d => rU.exec(d))
-}).add('<regex>/v', () => {
-  elements.forEach(d => rV.exec(d))
-});
-
-await bench.run();
-
-console.log(bench.name)
-console.table(bench.table())
-
+await run();
