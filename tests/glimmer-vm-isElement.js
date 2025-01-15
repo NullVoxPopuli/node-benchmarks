53a29d5ba34ca58fe2e086423710d8995a29f5f7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-15 11:34:31 -0500

    Also use tinybench

--- tests/glimmer-vm-isElement.js
+++ tests/glimmer-vm-isElement.js
@@ -1,25 +1,76 @@
 import { Suite, chartReport } from 'bench-node';
+import { Bench } from 'tinybench'
 
-const suite = new Suite({
+const workerSuite = new Suite({
   reporter: chartReport,
   // requires no shared data between workers
   useWorkers: true,
 });
 
-suite.add('<regex>', () => {
+const suite = new Suite({
+  reporter: chartReport,
+});
+
+const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
+const rDefault = /^<([\d\-a-z][\d\-A-Za-z]*)>$/;
+const rU = /^<([\d\-a-z][\d\-A-Za-z]*)>$/u;
+const rV = /^<([\d\-a-z][\d\-A-Za-z]*)>$/v;
+
+
+workerSuite.add('<regex>', () => {
   const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
   const rDefault = /^<([\d\-a-z][\d\-A-Za-z]*)>$/;
   elements.forEach(d => rDefault.exec(d))
 });
-suite.add('<regex>/u', () => {
+workerSuite.add('<regex>/u', () => {
   const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
   const rU = /^<([\d\-a-z][\d\-A-Za-z]*)>$/u;
   elements.forEach(d => rU.exec(d))
 });
-suite.add('<regex>/v', () => {
+workerSuite.add('<regex>/v', () => {
   const elements = ['<div>', '<web-component>', '<camelCase>', '<divÑ>', '<web-compÑonent>', 'not a element', '<Component>'];
   const rV = /^<([\d\-a-z][\d\-A-Za-z]*)>$/v;
   elements.forEach(d => rV.exec(d))
 });
 
-suite.run()
+
+suite.add('<regex>', () => {
+  elements.forEach(d => rDefault.exec(d))
+});
+suite.add('<regex>/u', () => {
+  elements.forEach(d => rU.exec(d))
+});
+suite.add('<regex>/v', () => {
+  elements.forEach(d => rV.exec(d))
+});
+
+console.log('\n\nIn workers');
+await workerSuite.run()
+
+console.log('\n\nNot in workers');
+await suite.run();
+
+console.log('\n\nwith tinybench');
+const bench = new Bench({
+  name: 'regex flag comparison', time: 100,
+  setup: (_task, mode) => {
+    // Run the garbage collector before warmup at each cycle
+    if (mode === 'warmup' && typeof globalThis.gc === 'function') {
+      globalThis.gc()
+    }
+  },
+});
+
+bench.add('<regex>', () => {
+  elements.forEach(d => rDefault.exec(d))
+}).add('<regex>/u', () => {
+  elements.forEach(d => rU.exec(d))
+}).add('<regex>/v', () => {
+  elements.forEach(d => rV.exec(d))
+});
+
+await bench.run();
+
+console.log(bench.name)
+console.table(bench.table())
+
